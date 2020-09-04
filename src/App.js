77a9182b51765dda998b808a6aa95ce4849c12b8
agwenchez <agweraenock76@gmail.com>
2020-09-04 18:58:09 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Lets do this MF</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSwGKMDvYUbO2BHGYKUdplgI81aLXgtVA",
+    authDomain: "whatsapp-clone-e022b.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-e022b.firebaseio.com",
+    projectId: "whatsapp-clone-e022b",
+    storageBucket: "whatsapp-clone-e022b.appspot.com",
+    messagingSenderId: "767619768520",
+    appId: "1:767619768520:web:20309b5bffb961e16545fd"
+  };
